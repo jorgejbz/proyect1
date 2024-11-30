16bb98ea2364174f9d0cce96797768ab63d54395
Jorge Juarez B <jorgejb234@gmail.com>
2024-11-30 02:32:15 -0600

    ICONO EN VENTANA MINI

--- resources/views/admin/layout/layout.blade.php
+++ resources/views/admin/layout/layout.blade.php
@@ -16,8 +16,8 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="csrf-token" content="{{ csrf_token() }}"/>
-  <link rel="icon" href="{{ asset('public/admin/img/favicon.ico') }}" type="image/x-icon">
-  <title>Ecotrack | Inicio</title>
+  <link rel="icon" href="{{ asset('admin/img/EcoTrack.jpg') }}" type="image/x-icon">
+  <title>Ecotrack | Web </title>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
